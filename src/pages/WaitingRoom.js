7163edfa1e55f4d3ae7dca61afc0dbe8dc15a8b2
yyjlincoln <yyjlincoln@gmail.com>
2021-12-19 14:51:11 +1100

    Update: Cleaned up the code a bit

--- src/pages/WaitingRoom.js
+++ src/pages/WaitingRoom.js
@@ -15,31 +15,11 @@ class WaitingRoom extends React.Component {
         // Check for the validity of current session
 
         Connection.resumeSession().then((sessionInfo) => {
-            console.log("ResumeSession")
-            if (sessionInfo.activeChat) {
-                this.setState({
-                    loading: `Connecting to chat # ${sessionInfo.activeChat}`
-                })
-                Connection.joinChat(sessionInfo.activeChat).then(() => {
-
-                })
+            if (sessionInfo.chatId) {
+                this.joinChat(sessionInfo.chatId)
             } else {
-                this.setState({
-                    loading: "Matching you with another user..."
-                })
-                Connection.on('new-chat-found', (res) => {
-                    const { chatId } = res
-                    this.setState({
-                        loading: `Connecting to chat # ${chatId}`
-                    })
-                    Connection.joinChat(chatId).then(() => {
-
-                    })
-                })
-                Connection.emit("new-chat-request").then(() => {
-                    this.setState({
-                        loading: "Request sent successfully. Waiting for a match..."
-                    })
+                this.matchChat().then((chatId) => {
+                    this.joinChat(chatId)
                 }).catch((e) => {
                     Connection.destroySession(Connection.sessionId).then(() => {
                         window.$alert.present("We couldn't find a match for you.", `Try creating a new session. Message: ${e.message}`, [
@@ -53,13 +33,40 @@ class WaitingRoom extends React.Component {
                         ])
 
                     })
-
                 })
             }
         }).catch(() => {
             history.replace("/")
             window.$alert.present("Invalid Session", "You session is no longer active. Please create a new session.")
         })
+    }
+
+    joinChat(chatId) {
+        this.setState({
+            loading: `Connecting to chat # ${chatId}`
+        })
+        Connection.joinChat(chatId).then(() => {
+
+        })
+    }
+
+    async matchChat() {
+        return new Promise((resolve, reject) => {
+            this.setState({
+                loading: "Matching you with another user..."
+            })
+            Connection.on('new-chat-found', (res) => {
+                resolve(res.chatId)
+            })
+            Connection.emit("new-chat-request").then(() => {
+                this.setState({
+                    loading: "Request sent successfully. Waiting for a match..."
+                })
+            }).catch((e) => {
+                reject(e)
+            })
+        })
+
     }
 
     render() {
